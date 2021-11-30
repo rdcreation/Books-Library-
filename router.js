f8908f89e5e13f1865ddd8db4b265a328efcc3c5
“rdcreation” <rajeshdevaraj95@gmail.com>
2021-12-01 02:19:48 +0530

    first commit

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const Router  = require("koa-router");
 const router = new Router();
 
 
-const userdetails = require('./Controller/userDetails');
+const userdetails = require('./Controller/userDetails')
 const books = require('./Controller/books');
 
 
@@ -11,10 +11,14 @@ router
 .post('/adduser',userdetails.adduserdeatils)
 .get('/getuser',userdetails.getuserdeatils)
 .post('/addbook',books.addbooks)
+.get('/getbooks',books.getbooksdeatils)
+.get('/getbookname/:bookname',books.getbooks)
+.get('/getbook/:author',books.getauthor)
+.put('/updatebook',books.updatebook)
 
 module.exports=router;
 
-// 
+
 
 
 
